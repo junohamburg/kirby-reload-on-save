9496e6ea9b3475098e887ab4c9416c2ff8b0ef8d
Sebastian Eberlein <sebastian@juno-hamburg.com>
2023-09-12 14:29:13 +0200

    Simplify content save detection

--- index.js
+++ index.js
@@ -3,17 +3,10 @@ panel.plugin('junohamburg/reload-on-save', {
     if (!'BroadcastChannel' in window) return;
 
     const bc = new BroadcastChannel(this.panel.$urls.site);
-    let saveInProgress = false;
 
     Vue.$store.subscribeAction({
       after: (action, state) => {
         if (action.type === 'content/save') {
-          saveInProgress = true;
-        }
-
-        if (action.type === 'notification/success' && saveInProgress) {
-          saveInProgress = false;
-
           bc.postMessage('content/saved');
         }
       }
