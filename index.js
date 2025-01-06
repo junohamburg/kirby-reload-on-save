719008854c5b6311fef49b713df6059720d3e25a
Sebastian Eberlein <sebastian@juno-hamburg.com>
2025-01-06 11:13:24 +0100

    Add support for Kirby 5

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ panel.plugin('junohamburg/reload-on-save', {
   created(Vue) {
     if (!'BroadcastChannel' in window) return;
 
-    const bc = new BroadcastChannel(this.panel.$urls.site);
+    const bc = new BroadcastChannel(this.panel.urls.site);
 
-    Vue.$store.subscribeAction({
-      after: (action, state) => {
-        if (action.type === 'content/save') {
-          bc.postMessage('content/saved');
+    // Kirby 4
+    if (Vue.$store) {
+      Vue.$store.subscribeAction({
+        after: (action, state) => {
+          if (action.type === 'content/save') {
+            bc.postMessage('content/saved');
+          }
         }
-      }
-    });
+      });
+    }
+    // Kirby 5
+    else {
+      window.panel.events.on('model.update', () => {
+        bc.postMessage('content/saved');
+      });
+    }
   }
 });
